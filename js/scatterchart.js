a294fa60a4e65e47318dcd1176a68bc256ab7ff4
Forrest Wilson <forrest.wilson@me.com>
2018-01-31 15:15:11 +1300

    Interactive scatter chart

--- js/scatterchart.js
+++ js/scatterchart.js
@@ -23,14 +23,20 @@ $(document).ready(function() {
         
             var height = 400,
                 width = 600;
+
+            var maxX = d3.max(countX());
         
             var yScale = d3.scaleLinear()
                            .domain([0, d3.max(countY())])
                            .range([0, height]);
         
             var xScale = d3.scaleLinear()
-                           .domain([0, d3.max(countX())])
+                           .domain([0, maxX])
                            .range([0, width]);
+
+            var colorX = d3.scaleLinear()
+                          .domain([0, d3.max(countX()) / 2, d3.max(countX())])
+                          .range(["#3498DB", "#F1C40F", "#E74C3C"]);
         
             d3.select("body").append("svg")
                 .attr("width", width)
@@ -39,15 +45,35 @@ $(document).ready(function() {
                 .selectAll("circle")
                     .data(data)
                     .enter().append("circle")
-                        .style("fill", "lightblue")
-                        .style("stroke", "black")
+                        .classed("circle", true)
+                        .style("fill", function(d, i) {
+                            return colorX(d.x);
+                        })
                         .style("stroke-width", "2")
-                        .attr("r", "5")
-                        .attr("cx", function(d, i) {
+                        .attr("r", "10")
+                        .attr("cx", function(d) {
                             return xScale(d.x);
                         })
-                        .attr("cy", function(d, i) {
+                        .attr("cy", function(d) {
                             return height - yScale(d.y);
+                        })
+                        .on("mouseover", function() {
+                            d3.select(this)
+                                .style("opacity", 0.5);
+                        })
+                        .on("mouseout", function() {
+                            d3.select(this)
+                                .style("opacity", 1);
+                        })
+                        .on("click", function(d) {
+                            d3.select(".active")
+                                .classed("active", false);
+
+                            d3.select("#xVal").html("<p>Selected X Value: " + d.x + "</div>");
+                            d3.select("#yVal").html("<p>Selected Y Value: " + d.y + "</div>");
+
+                            d3.select(this)
+                                .classed("active", true);
                         })
                     .exit();
         }
